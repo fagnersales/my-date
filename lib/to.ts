a281bba74ef26e1a28d9f323a1685aa8e9646a89
Fagner Sales <fs.fagnersales@gmail.com>
2023-05-11 09:30:41 -0300

    Update to.ts

--- lib/to.ts
+++ lib/to.ts
@@ -3,6 +3,6 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 
-export const to = (date: MyDate, toDate: MyDate): string => {
+export const to = (date: MyDate | Date, toDate: MyDate | Date): string => {
   return dayjs(date).to(toDate)
 }
